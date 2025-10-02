f521414231b335497502ea103e193535a474ab64
Ethan CHAINTRON <e.chaintron@rubika-edu.com>
2025-10-02 19:24:32 +0200

    Bits convertor to octet, ko, Mo, Go and To

--- Projet Horde Survivor/Assets/Scripts/ClickerManager.cs
+++ Projet Horde Survivor/Assets/Scripts/ClickerManager.cs
@@ -6,8 +6,29 @@ using UnityEngine;
 public class ClickerManager : MonoBehaviour
 {
     [SerializeField] private int clicks;
-    [SerializeField] private int bits;
+    [SerializeField] private long bits;
     [SerializeField] private int clickPrice = 1;
+    
+    private string[]bitsMetric = new string[6]
+    {
+        "bits",
+        "octet",
+        "ko",
+        "Mo",
+        "Go",
+        "To"
+    };
+    private long[]bitsMetricModulo = new long[6]
+    {
+        1,
+        8,
+        1000,
+        1000000,
+        1000000000,
+        1000000000000
+    };
+    private int bitsMetricIndex = 0;
+    
     
     [SerializeField] GameObject autoClicker;
     [SerializeField] List<GameObject> autoClickers;
@@ -35,14 +56,43 @@ public class ClickerManager : MonoBehaviour
         {
             bits += clickPrice;
             clicks--;
+            ConvertBites();
             BitsDisplayUpdate();
             ClicksDisplayUpdate();
         }
     }
 
+    private void ConvertBites()
+    {
+        if (bits < 8)
+        {
+            bitsMetricIndex = 0;
+        }
+        else if (bits < 1000)
+        {
+            bitsMetricIndex = 1;
+        }
+        else if (bits < 1000000)
+        {
+            bitsMetricIndex = 2;
+        }
+        else if (bits < 1000000000)
+        {
+            bitsMetricIndex = 3;
+        }
+        else if (bits < 1000000000000)
+        {
+            bitsMetricIndex = 4;
+        }
+        else
+        {
+            bitsMetricIndex = 5;
+        }
+    }
+
     void BitsDisplayUpdate()
     {
-        bitsText.text = bits + " bits";
+        bitsText.text = bits / bitsMetricModulo[bitsMetricIndex] + " " + bitsMetric[bitsMetricIndex];
     }
     
     void ClicksDisplayUpdate()
