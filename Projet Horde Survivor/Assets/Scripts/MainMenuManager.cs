78a4d9b51f8bf28dc53b03312b744bdbf778508f
OV3RL0RDzr <158048709+OV3RL0RDzr@users.noreply.github.com>
2025-10-28 13:31:43 +0100

    v1 proto main menu + menu settings proto
    v1 proto main menu + menu settings proto (wip)

---
+++ Projet Horde Survivor/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+ public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene"); //modif la scene par scene de jeu finale
+    }
+
+    public void QuitGame() //FONCTIONNE UNIQUMENT SUR APLICATION BUILD
+    {
+        Application.Quit();
+    }
+}
---
+++ Projet Horde Survivor/Assets/Scripts/SettingManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingManager : MonoBehaviour
+{
+    public Image overlayImage;
+    public Slider brightnessSlider;
+
+    public Toggle fullscreenToggle;
+
+    private void Start()
+    {
+        brightnessSlider.value = 1f;
+        UpdateBrightness(brightnessSlider.value);
+        brightnessSlider.onValueChanged.AddListener(UpdateBrightness);
+
+        if (fullscreenToggle != null)
+            fullscreenToggle.isOn = Screen.fullScreen;
+
+        
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+ 
+    private void UpdateBrightness(float value)
+    {
+        float alpha = Mathf.Clamp01(1f - value);
+        overlayImage.color = new Color(0f, 0f, 0f, alpha);
+    }
+}
