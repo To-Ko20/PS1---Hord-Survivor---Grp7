84eaf23493d3e300a035a52aae17c38671fab8b8
Ethan CHAINTRON <e.chaintron@rubika-edu.com>
2025-11-18 18:23:02 +0100

    start poison zone need finish

--- Projet Horde Survivor/Assets/Scripts/PlayerController.cs
+++ Projet Horde Survivor/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
     public float playerSpeed = 1f;
     
     public bool canMove = true;
+    private float t = 1f;
+    private bool canApplyPoison = true;
+    
+    [SerializeField] private CircleCollider2D circleCollider2D;
 
     private Vector2 movement;
     
@@ -27,6 +31,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        circleCollider2D.radius = PlayerSkillHolderManager.Instance.magnetRadius;
     }
 
     void FixedUpdate()
@@ -34,6 +39,33 @@ public class PlayerController : MonoBehaviour
         if (canMove)
         {
             rb.MovePosition(rb.position + movement * (playerSpeed * Time.fixedDeltaTime));
+        }
+        Countdown();
+    }
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (PlayerSkillHolderManager.Instance.hasPoisonZone && canApplyPoison)
+        {
+            foreach (GameObject nmi in EnemyManager.Instance.activeEnemies)
+            {
+                if (collision.transform == nmi.transform)
+                {
+                    nmi.GetComponent<EnemyMovement>().EnemyTakeDamage(PlayerSkillHolderManager.Instance.poisonDamage);
+                    canApplyPoison = false;
+                }   
+            }
+        }
+    }
+
+    private void Countdown()
+    {
+        t -= Time.fixedDeltaTime;
+        
+        if (t <= 0)
+        {
+            t = 1;
+            canApplyPoison = true;
         }
     }
 }
