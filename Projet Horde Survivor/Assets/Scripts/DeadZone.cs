aea7072cc5fde75281d0dab1ae5f0f6b21393b44
Ethan CHAINTRON <e.chaintron@rubika-edu.com>
2025-12-30 03:00:09 +0100

    Dead Zone code

---
+++ Projet Horde Survivor/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,47 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float damage;
+    
+    [SerializeField] private bool inZone = false;
+    private float t = 0f;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.position == player.transform.position)
+        {
+            inZone = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.position == player.transform.position)
+        {
+            inZone = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Timer();
+    }
+
+    private void Timer()
+    {
+        t -= Time.fixedDeltaTime;
+
+        if (t <= 0f)
+        {
+            t = 1f;
+            if (inZone)
+            {
+                player.GetComponent<PlayerManager>().TakeDamage(damage);
+            }
+        }
+    }
+}
