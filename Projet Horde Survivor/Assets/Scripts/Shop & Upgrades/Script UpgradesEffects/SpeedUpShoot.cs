f3d662d9e454a60e7d037394668c12a259283104
François <f.dassonville@rubika-edu.com>
2026-01-05 14:47:24 +0100

    Balancing Part I

--- Projet Horde Survivor/Assets/Scripts/Shop & Upgrades/Script UpgradesEffects/SpeedUpShoot.cs
+++ Projet Horde Survivor/Assets/Scripts/Shop & Upgrades/Script UpgradesEffects/SpeedUpShoot.cs
@@ -4,6 +4,7 @@ public class SpeedUpShoot : MonoBehaviour
 {
     public void OnUpgradeBought()
     {
-        PlayerShoot.Instance.countdownToShoot /= 2;
+        PlayerShoot.Instance.countdownToShoot -= 0.0135f;
+        // vitesse de tir au niveau 50 est environ 3* supérieure au niveau 1
     }
 }
