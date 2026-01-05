d6f189fc99e67598072725650fe999c9698b676d
Ethan CHAINTRON <e.chaintron@rubika-edu.com>
2026-01-05 23:01:49 +0100

    Try smth for volume and fix bugs

--- Projet Horde Survivor/Assets/Scripts/MANAGERS/SoundManager.cs
+++ Projet Horde Survivor/Assets/Scripts/MANAGERS/SoundManager.cs
@@ -1,7 +1,11 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField] private GameObject clickTrigger;
     [SerializeField] private GameObject buyTrigger;
     
     public static SoundManager Instance;
@@ -17,6 +21,22 @@ public class SoundManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    void Start ()
+    {
+        var buttons = FindObjectsByType<Button>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        foreach (var b in buttons)	{		
+            UnityAction l = delegate { OnClick(); };
+            b.onClick.AddListener(l);
+        }
+    }
+    
+
+    void OnClick()
+    {
+        clickTrigger.SetActive(!clickTrigger.activeSelf);
+    }
+    
 
     public void PlayBuy()
     {
--- Projet Horde Survivor/Assets/Scripts/MANAGERS/VolumeController.cs
+++ Projet Horde Survivor/Assets/Scripts/MANAGERS/VolumeController.cs
@@ -7,15 +7,18 @@ public class VolumeController : MonoBehaviour
 {
     private FMOD.Studio.VCA _vcaController;
     [SerializeField] private string _vcaName;
+    [SerializeField] private Slider slider;
 
     void Start()
     {
         _vcaController = FMODUnity.RuntimeManager.GetVCA("vca:/" + _vcaName);
-        SetVolume(GetComponent<Slider>());
+        slider.value = SaveManager.Instance.inventory.volumes[_vcaName];
     }
 
     public void SetVolume(Slider volume)
     {
         _vcaController.setVolume(volume.value);
+        SaveManager.Instance.inventory.volumes[_vcaName] = volume.value;
+        SaveManager.Instance.SaveToJson();
     }
 }
